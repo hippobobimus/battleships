655568ac84d23573292d1281abff1e8ec9861f39
Rob Masters <rjcmasters@gmail.com>
2022-03-11 18:06:22 +0000

    added method to place ships on gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,8 +1,34 @@
+import Ship from './ship.js';
+
 class Gameboard {
   constructor(size) {
     this.size = size;
-    this.missedAttacks = new Array(size).fill(new Array(size).fill(false));
-    this.ships = new Array(size).fill(new Array(size).fill(null));
+    this.missedAttacks = [];
+    this.ships = [];
+
+    for (let r = 0; r < size; r += 1) {
+      let missedAttacksRow = [];
+      let shipsRow = [];
+
+      for (let c = 0; c < size; c += 1) {
+        missedAttacksRow.push(false);
+        shipsRow.push(null);
+      }
+
+      this.missedAttacks.push(missedAttacksRow);
+      this.ships.push(shipsRow);
+    }
+  }
+
+  placeShip(length, position, isHorizontal) {
+    let ship = new Ship(length);
+
+    for (let i = 0; i < length; i += 1) {
+      let rowOffset = isHorizontal ? 0 : i;
+      let colOffset = isHorizontal ? i : 0;
+
+      this.ships[position.row + rowOffset][position.col + colOffset] = ship;
+    }
   }
 }
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,7 +1,7 @@
 import Gameboard from './gameboard.js';
 
 describe('gameboard', () => {
-  test.only('gameboard initialisation', () => {
+  test('gameboard initialisation', () => {
     let size = 10;
     let g = new Gameboard(size);
 
@@ -15,8 +15,55 @@ describe('gameboard', () => {
     }
   });
 
-  test.todo('horizontal ship placement');
-  test.todo('vertical ship placement');
+  test('check correct horizontal ship placement', () => {
+    let size = 10;
+    let g = new Gameboard(size);
+
+    let length = 3;
+    let pos = { row: 3, col: 7 };
+    let isHor = true;
+    g.placeShip(length, pos, isHor);
+
+    for (let row = 0; row < size; row += 1) {
+      for (let col = 0; col < size; col += 1) {
+        if (
+          row === pos.row &&
+          col >= pos.col &&
+          col < pos.col + length
+        ) {
+          expect(g.ships[row][col]).not.toBeNull();
+        } else {
+          expect(g.ships[row][col]).toBeNull();
+        }
+      }
+    }
+  });
+
+  test('check correct vertical ship placement', () => {
+    let size = 10;
+    let g = new Gameboard(size);
+
+    let length = 5;
+    let pos = { row: 2, col: 9 };
+    let isHor = false;
+    g.placeShip(length, pos, isHor);
+
+    for (let row = 0; row < size; row += 1) {
+      for (let col = 0; col < size; col += 1) {
+        if (
+          row >= pos.row &&
+          row < pos.row + length &&
+          col === pos.col
+        ) {
+          expect(g.ships[row][col]).not.toBeNull();
+        } else {
+          expect(g.ships[row][col]).toBeNull();
+        }
+      }
+    }
+  });
+
+  test.todo('check out of bounds ship placement');
   test.todo('receive attack');
   test.todo('all ships sunk');
 });
