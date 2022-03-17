1cfbaa494f2e1dfa6bcfe65f8d2441357a63cd96
Rob Masters <rjcmasters@gmail.com>
2022-03-17 16:57:01 +0000

    added methods for displaying a hit or miss icon on the board view

--- src/gameboard-view.js
+++ src/gameboard-view.js
@@ -1,13 +1,18 @@
 import './gameboard.css';
+import HitImage from './assets/game-graphics/hit.svg';
+import MissImage from './assets/game-graphics/miss.svg';
 import GameEvent from './game-event.js';
 import Position from './position.js';
 
 class GameboardView {
   constructor(root, gameboardSize) {
+    this.root = root;
+    this.gameboardSize = gameboardSize;
+
     this.moveInputEvent = new GameEvent();
 
-    let board = document.createElement('div');
-    board.classList.add('board');
+    this.board = document.createElement('div');
+    this.board.classList.add('board');
 
     for (let row = 0; row < gameboardSize; row += 1) {
       for (let col = 0; col < gameboardSize; col += 1) {
@@ -21,11 +26,31 @@ class GameboardView {
           { once: true }
         );
 
-        board.appendChild(cell);
+        this.board.appendChild(cell);
       }
     }
 
-    root.appendChild(board);
+    this.root.appendChild(this.board);
+  }
+
+  displayHit(position) {
+    this.#displayCellImage(HitImage, position);
+  }
+
+  displayMiss(position) {
+    this.#displayCellImage(MissImage, position);
+  }
+
+  #displayCellImage(image, position) {
+    let img = document.createElement('img');
+
+    img.classList.add('hit');
+    img.src = image;
+
+    let cells = this.board.querySelectorAll('.cell');
+
+    let idx = this.gameboardSize * position.row + position.col;
+    cells[idx].appendChild(img);
   }
 }
 
