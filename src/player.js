9639808ce935edd354c2f091320f172d219bff0c
Rob Masters <rjcmasters@gmail.com>
2022-03-18 16:34:19 +0000

    added simple random ai move generation

--- src/player.js
+++ src/player.js
@@ -1,9 +1,36 @@
 import Gameboard from './gameboard.js';
+import Position from './position.js';
 
 class Player {
+  #previousMoves;
+
   constructor(gameboardSize, isAi) {
     this.gameboard = new Gameboard(gameboardSize);
     this.isAi = isAi;
+
+    this.#previousMoves = [];
+  }
+
+  getAiMove() {
+    if (!this.isAi) {
+      throw Error('this is not an ai player');
+    }
+
+    let result = Position.random(this.gameboard.size);
+
+    while (this.#isRepeatedMove(result)) {
+      result = Position.random(this.gameboard.size);
+    }
+
+    this.#previousMoves.push(result);
+
+    return result;
+  }
+
+  #isRepeatedMove(position) {
+    return this.#previousMoves.some(
+      (prev) => prev.row === position.row && prev.col === position.col
+    );
   }
 }
 
--- src/player.test.js
+++ src/player.test.js
@@ -15,9 +15,43 @@ describe('player', () => {
     let p2 = new Player(expected2.gameboardSize, expected2.isAi);
 
     expect(p1.gameboard.size).toBe(expected1.gameboardSize);
-    expect(p1.isAi).toBeFalsy;
+    expect(p1.isAi).toBeFalsy();
 
     expect(p2.gameboard.size).toBe(expected2.gameboardSize);
-    expect(p2.isAi).toBeTruthy;
+    expect(p2.isAi).toBeTruthy();
+  });
+
+  test('non-ai players cannot generate an ai move', () => {
+    let gameboardSize = 9;
+    let isAi = false;
+
+    let p = new Player(gameboardSize, isAi);
+
+    expect(() => p.getAiMove()).toThrow('this is not an ai player');
+  });
+
+  test('ai move generation is unique and in bounds', () => {
+    let gameboardSize = 5;
+    let isAi = true;
+
+    let p = new Player(gameboardSize, isAi);
+
+    let moves = [];
+
+    for (let i = 0; i < gameboardSize * gameboardSize; i += 1) {
+      let m = p.getAiMove();
+
+      expect(m.row).toBeGreaterThanOrEqual(0);
+      expect(m.row).toBeLessThan(gameboardSize);
+
+      expect(m.col).toBeGreaterThanOrEqual(0);
+      expect(m.col).toBeLessThan(gameboardSize);
+
+      expect(
+        moves.some((elem) => elem.row === m.row && elem.col === m.col)
+      ).toBeFalsy();
+
+      moves.push(m);
+    }
   });
 });
