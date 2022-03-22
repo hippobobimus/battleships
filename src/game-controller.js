1477d90550b268ca416532b268ed0e010d40a7ea
Rob Masters <rjcmasters@gmail.com>
2022-03-22 16:53:31 +0000

    updated game control to include new fading and messaging options

--- src/game-controller.js
+++ src/game-controller.js
@@ -1,5 +1,6 @@
 import GameboardView from './gameboard-view.js';
 import Player from './player.js';
+import TickerView from './ticker-view.js';
 
 class GameController {
   static BOARD_SIZE = 10;
@@ -13,16 +14,20 @@ class GameController {
     this.isGameover = false;
     this.isPlayerTurn = true;
 
+    let root = document.getElementById('content');
+    let boards = document.getElementById('boards');
+
     this.playerView = new GameboardView(
-      document.getElementById('content'),
+      boards,
       GameController.BOARD_SIZE,
-      'Your Zone',
+      'Your Zone'
     );
     this.computerView = new GameboardView(
-      document.getElementById('content'),
+      boards,
       GameController.BOARD_SIZE,
-      'The Admiral\'s Zone',
+      "The Admiral's Zone"
     );
+    this.tickerView = new TickerView(root);
 
     // place ships.
     this.player.gameboard.placeShipsRandomly(GameController.SHIP_LENGTHS);
@@ -49,6 +54,7 @@ class GameController {
   load() {
     this.playerView.load();
     this.computerView.load();
+    this.tickerView.load();
 
     // display all of the player's ship positions.
     this.player.gameboard.ships.forEach((ship) => {
@@ -58,6 +64,8 @@ class GameController {
         ship.isHorizontal
       );
     });
+
+    this.playerView.fade();
   }
 
   #takeComputerTurn() {
@@ -66,8 +74,6 @@ class GameController {
     }
 
     this.player.gameboard.receiveAttack(this.computer.getAiMove());
-
-    this.isPlayerTurn = true;
   }
 
   onPlayerAttackInputEvent = async (position) => {
@@ -75,45 +81,101 @@ class GameController {
       return;
     }
 
+    this.tickerView.clear();
+
     this.isPlayerTurn = false;
     this.computer.gameboard.receiveAttack(position);
 
-    setTimeout(() => this.#takeComputerTurn(), 1000);
+    setTimeout(() => {
+      if (!this.isGameover) {
+        this.computerView.fade();
+        this.playerView.clearFade();
+      }
+    }, 2000);
+
+    setTimeout(() => {
+      this.#takeComputerTurn();
+      setTimeout(() => {
+        if (!this.isGameover) {
+          this.computerView.clearFade();
+          this.playerView.fade();
+          this.tickerView.displayMessage(
+            "Select a target in the Admiral's zone"
+          );
+        }
+        this.isPlayerTurn = true;
+      }, 2000);
+    }, 2500);
   };
 
   onPlayerBoardHitEvent = (position) => {
     this.playerView.displayHit(position);
+    this.playerView.displayBoardMessage('Hit!');
+    setTimeout(() => this.playerView.hideBoardMessage(), 1500);
   };
 
   onComputerBoardHitEvent = (position) => {
     this.computerView.displayHit(position);
+    this.computerView.displayBoardMessage('Hit!');
+    setTimeout(() => this.computerView.hideBoardMessage(), 1500);
   };
 
   onPlayerBoardMissEvent = (position) => {
     this.playerView.displayMiss(position);
+    this.playerView.displayBoardMessage('Miss!');
+    setTimeout(() => this.playerView.hideBoardMessage(), 1500);
   };
 
   onComputerBoardMissEvent = (position) => {
     this.computerView.displayMiss(position);
+    this.computerView.displayBoardMessage('Miss!');
+    setTimeout(() => this.computerView.hideBoardMessage(), 1500);
   };
 
-  onPlayerBoardSunkEvent = () => {
-    console.log('player ship sunk!');
-    // TODO
+  onPlayerBoardSunkEvent = (hitPosition) => {
+    this.playerView.displayHit(hitPosition);
+    this.playerView.displayBoardMessage('Ship sunk!');
+    setTimeout(() => this.playerView.hideBoardMessage(), 1500);
   };
 
-  onComputerBoardSunkEvent = (position, length, isHorizontal) => {
-    this.computerView.displayShip(position, length, isHorizontal);
+  onComputerBoardSunkEvent = (
+    hitPosition,
+    shipPosition,
+    shipLength,
+    shipIsHor
+  ) => {
+    this.computerView.displayShip(shipPosition, shipLength, shipIsHor);
+    this.computerView.displayHit(hitPosition);
+    this.computerView.displayBoardMessage('Ship sunk!');
+    setTimeout(() => this.computerView.hideBoardMessage(), 1500);
   };
 
-  onPlayerBoardAllShipsSunkEvent = () => {
-    console.log('player loses!');
+  onPlayerBoardAllShipsSunkEvent = (
+    hitPosition,
+    shipPosition,
+    shipLength,
+    shipIsHor
+  ) => {
     this.isGameover = true;
+    this.tickerView.displayMessage('YOU HAVE BEEN DEFEATED');
+    this.playerView.displayShip(shipPosition, shipLength, shipIsHor);
+    this.playerView.displayHit(hitPosition);
+    this.playerView.displayBoardMessage('Fleet sunk!');
+    setTimeout(() => this.playerView.fade(), 2500);
   };
 
-  onComputerBoardAllShipsSunkEvent = () => {
-    console.log('computer loses!');
+  onComputerBoardAllShipsSunkEvent = (
+    hitPosition,
+    shipPosition,
+    shipLength,
+    shipIsHor
+  ) => {
     this.isGameover = true;
+    this.tickerView.displayMessage('YOU ARE VICTORIOUS');
+    this.computerView.displayShip(shipPosition, shipLength, shipIsHor);
+    this.computerView.displayHit(hitPosition);
+    this.computerView.displayBoardMessage('Fleet sunk!');
+    setTimeout(() => this.computerView.fade(), 2500);
   };
 }
 
