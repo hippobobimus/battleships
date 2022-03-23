66757ff9c2b77a4039b414963483385d8f7683a0
Rob Masters <rjcmasters@gmail.com>
2022-03-23 11:52:35 +0000

    added a method to generate a play again button

--- src/game-view.js
+++ src/game-view.js
@@ -1,9 +1,13 @@
+import './button.css';
+import GameEvent from './game-event.js';
 import GameboardView from './gameboard-view.js';
 import TickerView from './ticker-view.js';
 
 class GameView {
+  #root;
+
   constructor(root, boardSize, playerTitleStr, computerTitleStr) {
-    this.root = root;
+    this.#root = root;
 
     this.boards = document.createElement('div');
     this.boards.id = 'boards';
@@ -11,14 +15,26 @@ class GameView {
     this.player = new GameboardView(this.boards, boardSize, playerTitleStr);
     this.computer = new GameboardView(this.boards, boardSize, computerTitleStr);
     this.ticker = new TickerView(root);
+
+    this.resetEvent = new GameEvent();
   }
 
   load() {
-    this.root.appendChild(this.boards);
+    this.#root.appendChild(this.boards);
 
     this.player.load();
     this.computer.load();
     this.ticker.load();
+  }
+
+  displayPlayAgain() {
+    let btn = document.createElement('div');
+
+    btn.classList.add('btn');
+    btn.innerText = 'Play Again';
+    btn.addEventListener('click', () => this.resetEvent.trigger());
+
+    this.#root.appendChild(btn);
   }
 }
 
