eadb6d9762caa0f4449b17e5450696a41845f3d6
Rob Masters <rjcmasters@gmail.com>
2022-03-23 10:21:02 +0000

    created a start screen view

---
+++ src/start-view.js
@@ -0,0 +1,34 @@
+import './start.css';
+import GameEvent from './game-event.js';
+import RobotImage from './assets/game-graphics/admiral.svg';
+
+class StartView {
+  #root;
+
+  constructor(root) {
+    this.#root = root;
+    this.startEvent = new GameEvent();
+  }
+
+  load() {
+    let img = document.createElement('img');
+    let tagline = document.createElement('h2');
+    let startBtn = document.createElement('div');
+
+    img.classList.add('admiral');
+    img.src = RobotImage;
+
+    tagline.classList.add('tagline');
+    tagline.innerText = 'Can you defeat Admiral Gizmotron?';
+
+    startBtn.classList.add('start-btn');
+    startBtn.innerText = 'Start';
+    startBtn.addEventListener('click', () => this.startEvent.trigger());
+
+    this.#root.appendChild(tagline);
+    this.#root.appendChild(img);
+    this.#root.appendChild(startBtn);
+  }
+}
+
+export default StartView;
